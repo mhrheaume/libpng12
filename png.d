f9c7e4f6944e5366af22c66bc937e04ff720b53f
Matthew Rheaume <mhrheaume@gmail.com>
2014-06-06 22:25:11 -0400

    Added debug constructs (commented out) and some extern arrays.

--- png.d
+++ png.d
@@ -611,7 +611,55 @@ enum png_write_status_ptr_NULL = cast(png_write_status_ptr)null;
 
 // TODO
 // /* Variables declared in png.c - only it needs to define PNG_NO_EXTERN */
-// #if !defined(PNG_NO_EXTERN) || defined(PNG_ALWAYS_EXTERN)
+version (PNG_NO_EXTERN) {
+} else version (PNG_ALWAYS_EXTERN) {
+   version(PNG_USE_GLOBAL_ARRAYS) {
+      /* Need room for 99.99.99beta99z */
+      extern const char png_libpng_ver[18];
+   } else {
+      @property auto png_libpng_ver() {
+         return png_libpng_ver(null);
+      }
+   } // PNG_USE_GLOBAL_ARRAYS
+
+   version(PNG_USE_GLOBAL_ARRAYS) {
+      /* This was removed in version 1.0.5c */
+      /* Structures to facilitate easy interlacing.  See png.c for more details */
+      extern const int png_pass_start[7];
+      extern const int png_pass_inc[7];
+      extern const int png_pass_ystart[7];
+      extern const int png_pass_yinc[7];
+      extern const int png_pass_mask[7];
+      extern const int png_pass_dsp_mask[7];
+      /* This isn't currently used.  If you need it, see png.c for more details.
+      extern const int png_pass_height[7];
+      */
+   }
+} else {
+   version(PNG_USE_GLOBAL_ARRAYS) {
+      /* Need room for 99.99.99beta99z */
+      extern const char png_libpng_ver[18];
+   } else {
+      @property auto png_libpng_ver() {
+         return png_libpng_ver(null);
+      }
+   } // PNG_USE_GLOBAL_ARRAYS
+
+   version(PNG_USE_GLOBAL_ARRAYS) {
+      /* This was removed in version 1.0.5c */
+      /* Structures to facilitate easy interlacing.  See png.c for more details */
+      extern const int png_pass_start[7];
+      extern const int png_pass_inc[7];
+      extern const int png_pass_ystart[7];
+      extern const int png_pass_yinc[7];
+      extern const int png_pass_mask[7];
+      extern const int png_pass_dsp_mask[7];
+      /* This isn't currently used.  If you need it, see png.c for more details.
+      extern const int png_pass_height[7];
+      */
+   }
+} // !PNG_ALWAYS_EXTERN
+
 // /* Version information for C files, stored in png.c.  This had better match
 //  * the version above.
 //  */
@@ -2838,6 +2886,113 @@ version (PNG_INFO_IMAGE_SUPPORTED) {
          int transforms,
          png_voidp params);
 } // PNG_INFO_IMAGE_SUPPORTED
+
+// /* Define PNG_DEBUG at compile time for debugging information.  Higher
+//  * numbers for PNG_DEBUG mean more debugging information.  This has
+//  * only been added since version 0.95 so it is not implemented throughout
+//  * libpng yet, but more support will be added as needed.
+//  */
+// #ifdef PNG_DEBUG
+// #if (PNG_DEBUG > 0)
+// #if !defined(PNG_DEBUG_FILE) && defined(_MSC_VER)
+// #include <crtdbg.h>
+// #if (PNG_DEBUG > 1)
+// #ifndef _DEBUG
+// #  define _DEBUG
+// #endif
+// #ifndef png_debug
+// #define png_debug(l,m)  _RPT0(_CRT_WARN,m PNG_STRING_NEWLINE)
+// #endif
+// #ifndef png_debug1
+// #define png_debug1(l,m,p1)  _RPT1(_CRT_WARN,m PNG_STRING_NEWLINE,p1)
+// #endif
+// #ifndef png_debug2
+// #define png_debug2(l,m,p1,p2) _RPT2(_CRT_WARN,m PNG_STRING_NEWLINE,p1,p2)
+// #endif
+// #endif
+// #else /* PNG_DEBUG_FILE || !_MSC_VER */
+// #ifndef PNG_DEBUG_FILE
+// #define PNG_DEBUG_FILE stderr
+// #endif /* PNG_DEBUG_FILE */
+//
+// #if (PNG_DEBUG > 1)
+// /* Note: ["%s"m PNG_STRING_NEWLINE] probably does not work on non-ISO
+//  * compilers.
+//  */
+// #  ifdef __STDC__
+// #    ifndef png_debug
+// #      define png_debug(l,m) \
+//        { \
+//        int num_tabs=l; \
+//        fprintf(PNG_DEBUG_FILE,"%s"m PNG_STRING_NEWLINE,(num_tabs==1 ? "\t" : \
+//          (num_tabs==2 ? "\t\t":(num_tabs>2 ? "\t\t\t":"")))); \
+//        }
+// #    endif
+// #    ifndef png_debug1
+// #      define png_debug1(l,m,p1) \
+//        { \
+//        int num_tabs=l; \
+//        fprintf(PNG_DEBUG_FILE,"%s"m PNG_STRING_NEWLINE,(num_tabs==1 ? "\t" : \
+//          (num_tabs==2 ? "\t\t":(num_tabs>2 ? "\t\t\t":""))),p1); \
+//        }
+// #    endif
+// #    ifndef png_debug2
+// #      define png_debug2(l,m,p1,p2) \
+//        { \
+//        int num_tabs=l; \
+//        fprintf(PNG_DEBUG_FILE,"%s"m PNG_STRING_NEWLINE,(num_tabs==1 ? "\t" : \
+//          (num_tabs==2 ? "\t\t":(num_tabs>2 ? "\t\t\t":""))),p1,p2); \
+//        }
+// #    endif
+// #  else /* __STDC __ */
+// #    ifndef png_debug
+// #      define png_debug(l,m) \
+//        { \
+//        int num_tabs=l; \
+//        char format[256]; \
+//        snprintf(format,256,"%s%s%s",(num_tabs==1 ? "\t" : \
+//          (num_tabs==2 ? "\t\t":(num_tabs>2 ? "\t\t\t":""))), \
+//          m,PNG_STRING_NEWLINE); \
+//        fprintf(PNG_DEBUG_FILE,format); \
+//        }
+// #    endif
+// #    ifndef png_debug1
+// #      define png_debug1(l,m,p1) \
+//        { \
+//        int num_tabs=l; \
+//        char format[256]; \
+//        snprintf(format,256,"%s%s%s",(num_tabs==1 ? "\t" : \
+//          (num_tabs==2 ? "\t\t":(num_tabs>2 ? "\t\t\t":""))), \
+//          m,PNG_STRING_NEWLINE); \
+//        fprintf(PNG_DEBUG_FILE,format,p1); \
+//        }
+// #    endif
+// #    ifndef png_debug2
+// #      define png_debug2(l,m,p1,p2) \
+//        { \
+//        int num_tabs=l; \
+//        char format[256]; \
+//        snprintf(format,256,"%s%s%s",(num_tabs==1 ? "\t" : \
+//          (num_tabs==2 ? "\t\t":(num_tabs>2 ? "\t\t\t":""))), \
+//          m,PNG_STRING_NEWLINE); \
+//        fprintf(PNG_DEBUG_FILE,format,p1,p2); \
+//        }
+// #    endif
+// #  endif /* __STDC __ */
+// #endif /* (PNG_DEBUG > 1) */
+//
+// #endif /* _MSC_VER */
+// #endif /* (PNG_DEBUG > 0) */
+// #endif /* PNG_DEBUG */
+// #ifndef png_debug
+// #define png_debug(l, m)
+// #endif
+// #ifndef png_debug1
+// #define png_debug1(l, m, p1)
+// #endif
+// #ifndef png_debug2
+// #define png_debug2(l, m, p1, p2)
+// #endif
 
 png_charp png_get_copyright(png_structp png_ptr);
 png_charp png_get_header_ver(png_structp png_ptr);
